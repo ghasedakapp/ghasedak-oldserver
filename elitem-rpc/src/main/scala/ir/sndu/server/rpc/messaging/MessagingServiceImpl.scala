759f7dd007633afbbe46271a01ac0c6912f8506a
AmirHossein Javan <amsjavan@gmail.com>
2018-04-14 02:51:57 +0430

    feat(rpc): Implements initial load history rpc

--- elitem-rpc/src/main/scala/ir/sndu/server/rpc/messaging/MessagingServiceImpl.scala
+++ elitem-rpc/src/main/scala/ir/sndu/server/rpc/messaging/MessagingServiceImpl.scala
@@ -1,10 +1,14 @@
 package ir.sndu.server.rpc.messaging
 
+import java.time.{ Instant, LocalDateTime, ZoneId }
+
 import akka.actor.ActorSystem
 import akka.event.{ Logging, LoggingAdapter }
+import com.google.protobuf.CodedInputStream
 import ir.sndu.persist.db.PostgresDb
+import ir.sndu.persist.repo.history.HistoryMessageRepo
 import ir.sndu.server.messaging.MessagingServiceGrpc.MessagingService
-import ir.sndu.server.messaging.{ RequestSendMessage, ResponseVoid }
+import ir.sndu.server.messaging._
 import ir.sndu.server.peer.ApiPeer
 import ir.sndu.server.rpc.auth.AuthHelper
 import ir.sndu.server.user.UserExtension
@@ -18,12 +22,28 @@ class MessagingServiceImpl(implicit system: ActorSystem) extends MessagingServic
   implicit protected val log: LoggingAdapter = Logging.getLogger(system, this)
   protected val userExt = UserExtension(system)
 
-  override def sendMessage(request: RequestSendMessage): Future[ResponseVoid] = {
+  override def sendMessage(request: RequestSendMessage): Future[ResponseVoid] =
     authorize(request.token) { userId =>
       val (outPeer, randomId, message, _) = RequestSendMessage.unapply(request).get
 
       userExt.sendMessage(userId, outPeer.map(p => ApiPeer(p.`type`, p.id)).get, randomId, message.get).map(
         _ => ResponseVoid())
     }
-  }
+
+  override def loadHistory(request: RequestLoadHistory): Future[ResponseLoadHistory] =
+    authorize(request.token) { userId =>
+      val (peer, date, limit, _) = RequestLoadHistory.unapply(request).get
+      db.run(HistoryMessageRepo.findAfter(
+        userId,
+        ApiPeer(peer.get.`type`, peer.get.id),
+        LocalDateTime.ofInstant(Instant.ofEpochMilli(date), ZoneId.systemDefault()),
+        limit)) map { history =>
+        history.map(msg => ApiMessageContainer(
+          msg.senderUserId,
+          msg.randomId,
+          msg.date.atZone(ZoneId.systemDefault()).toInstant.toEpochMilli,
+          Some(ApiMessage().mergeFrom(CodedInputStream.newInstance(msg.messageContentData)))))
+
+      } map (ResponseLoadHistory(_))
+    }
 }
--- elitem-test/src/test/scala/ir/sndu/server/messaging/MessagingSpec.scala
+++ elitem-test/src/test/scala/ir/sndu/server/messaging/MessagingSpec.scala
@@ -1,5 +1,7 @@
 package ir.sndu.server.messaging
 
+import java.time.Instant
+
 import ir.sndu.server.GrpcBaseSuit
 import ir.sndu.server.auth.AuthHelper
 import ir.sndu.server.peer.{ ApiOutPeer, ApiPeerType }
@@ -10,6 +12,7 @@ class MessagingSpec extends GrpcBaseSuit
   with AuthHelper {
 
   "send message" should "" in sendMessage
+  "load history" should "" in loadHistory
 
   def sendMessage(): Unit = {
     val UserInfo(user1, token1, _) = createUser()
@@ -21,6 +24,33 @@ class MessagingSpec extends GrpcBaseSuit
       Some(ApiMessage().withTextMessage(ApiTextMessage("salam"))),
       token1)
     messagingStub.sendMessage(request) shouldBe ResponseVoid()
+  }
+
+  def loadHistory: Unit = {
+    val UserInfo(user1, token1, _) = createUser()
+    val UserInfo(user2, token2, _) = createUser()
+
+    val outPeer2 = ApiOutPeer(ApiPeerType.Private, user2.id)
+
+    def befrest(msg: ApiMessage): Unit = {
+      messagingStub.sendMessage(RequestSendMessage(
+        Some(outPeer2),
+        Random.nextLong(),
+        Some(msg),
+        token1))
+    }
+
+    val msg1 = ApiMessage().withTextMessage(ApiTextMessage("salam1"))
+    val msg2 = ApiMessage().withTextMessage(ApiTextMessage("salam2"))
+    befrest(msg1)
+    befrest(msg2)
+
+    val rsp = messagingStub.loadHistory(RequestLoadHistory(
+      Some(outPeer2), Instant.now.minusSeconds(3600L).toEpochMilli, 10, token1))
+
+    rsp.history.size shouldBe 2
+    rsp.history.map(_.message.get) shouldBe Seq(msg1, msg2)
+
   }
 
 }
