e68f68433bbf39be7cb92e7cabd6173f1ca471c3
AmirHossein Javan <amsjavan@gmail.com>
2018-06-04 15:06:58 +0430

    refactor(cli): Creates class files

---
+++ elitem-cli/src/main/scala/ir/sndu/server/ElitemConsole.scala
@@ -0,0 +1,17 @@
+package ir.sndu.server
+
+object ElitemConsole {
+  def withColor(color: String)(f: => Unit): Unit = {
+    print(color)
+    f
+    print(Console.WHITE)
+  }
+
+  def withError(f: => Unit): Unit = withColor(Console.RED)(f)
+
+  def withWarning(f: => Unit): Unit = withColor(Console.RED)(f)
+
+  def withOutput(f: => Unit): Unit = withColor(Console.GREEN)(f)
+
+}
+
