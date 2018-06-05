20344b36d9077bba30b94101c27aeb732c5a94eb
amsjavan <amsjavan@gmail.com>
2018-06-05 12:29:46 +0430

    feat(cli): Creates clear cache command

---
+++ elitem-cli/src/main/scala/ir/sndu/server/command/ClearCache.scala
@@ -0,0 +1,17 @@
+package ir.sndu.server.command
+
+import java.io.File
+
+import ir.sndu.server.disk.FileHelper
+import picocli.CommandLine.Command
+
+@Command(
+  name = "clearcache",
+  description = Array("Clear DB"))
+class ClearCache extends Runnable {
+
+  override def run(): Unit = {
+    FileHelper.deleteRecursively(new File("./session"))
+  }
+}
+
