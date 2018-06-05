709341e6979202c93daa2bb9b8e79a932647b565
amsjavan <amsjavan@gmail.com>
2018-06-05 12:26:18 +0430

    feat(cli): Creates DB and File helper to use in commands

---
+++ elitem-cli/src/main/scala/ir/sndu/server/disk/DbHelper.scala
@@ -0,0 +1,32 @@
+package ir.sndu.server.disk
+
+import java.io._
+
+import ir.sndu.server.ElitemConsole.withColor
+import org.iq80.leveldb.{ DB, _ }
+import org.iq80.leveldb.impl.Iq80DBFactory._
+
+object DbHelper {
+
+  def leveldb(path: Option[String] = None)(f: DB => Unit) = {
+    val options = new Options()
+    options.createIfMissing(true)
+    val db = factory.open(new File(path.getOrElse(".") + "/session"), options)
+    try {
+      f(db)
+    } catch {
+      case e: Throwable =>
+        withColor(scala.Console.RED) {
+          System.err.println(s"Error: ${e.getMessage}")
+        }
+    } finally {
+      db.close()
+    }
+
+  }
+
+  implicit class RichLevelDB(db: DB) {
+    def put(key: String, value: String): Unit = db.put(bytes(key), bytes(value))
+    def get(key: String): Option[String] = Option(asString(db.get(bytes(key))))
+  }
+}
---
+++ elitem-cli/src/main/scala/ir/sndu/server/disk/FileHelper.scala
@@ -0,0 +1,12 @@
+package ir.sndu.server.disk
+
+import java.io.File
+
+object FileHelper {
+  def deleteRecursively(file: File): Unit = {
+    if (file.isDirectory)
+      file.listFiles.foreach(deleteRecursively)
+    if (file.exists && !file.delete)
+      throw new Exception(s"Unable to delete ${file.getAbsolutePath}")
+  }
+}
