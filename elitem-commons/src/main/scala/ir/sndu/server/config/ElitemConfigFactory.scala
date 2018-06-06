1e5c45c601606fc6b1b638427ab92027628f89dc
AmirHossein Javan <amsjavan@gmail.com>
2018-06-06 18:52:03 +0430

    feat(cli): Loads logback from conf folder if exists

--- elitem-commons/src/main/scala/ir/sndu/server/config/ElitemConfigFactory.scala
+++ elitem-commons/src/main/scala/ir/sndu/server/config/ElitemConfigFactory.scala
@@ -1,15 +1,49 @@
 package ir.sndu.server.config
 
 import java.io.File
+import java.nio.file.{ Files, Paths }
 
 import com.typesafe.config.{ Config, ConfigFactory }
+import ir.sndu.server.config.AppType.AppType
+
+object AppType extends Enumeration {
+  type AppType = Value
+  val Cli, Server, Test = Value
+}
 
 object ElitemConfigFactory {
-  def load(fileName: String): Config = {
-    val configPath = new File(".").getCanonicalPath + s"/conf/$fileName"
-    System.setProperty("config.file", configPath.toString)
+  def load(app: AppType): Config = {
+    val (configPath, logbackPath) = getPathes(app)
+
+    setConfig(configPath)
+    setLogback(logbackPath)
+
     System.setProperty("file.encoding", "UTF-8")
+
     ConfigFactory.load()
+  }
+
+  private def getPathes(app: AppType): (String, String) = {
+    val path = new File(".").getCanonicalPath
+    app match {
+      case AppType.Server => (path + "/conf/server.conf", path + "/conf/logback.xml")
+      case AppType.Cli => (path + "/conf/cli/cli.conf", path + "/conf/cli/logback.xml")
+      case AppType.Test => (path + "/conf/test/test.conf", path + "/conf/test/logback.xml")
+    }
+  }
+
+  private def setConfig(configPath: String): Unit = {
+    if (Files.exists(Paths.get(configPath)))
+      System.setProperty("config.file", configPath)
+    else
+      System.err.println(s"${Console.YELLOW} Config file does not exists in: $configPath")
+  }
+
+  private def setLogback(logbackPath: String): Unit = {
+    if (Files.exists(Paths.get(logbackPath)))
+      System.setProperty("logback.configurationFile", logbackPath)
+    else
+      System.err.println(s"${Console.YELLOW} Logback file does not exists in: $logbackPath")
   }
 
 }
