da5fd420acdb20fb2919eeb4fdb414d22024975f
AmirHossein Javan <amsjavan@gmail.com>
2018-06-22 11:36:01 +0430

    fix(cli): Clears specified session file

--- elitem-cli/src/main/scala/ir/sndu/server/command/cache/ClearCache.scala
+++ elitem-cli/src/main/scala/ir/sndu/server/command/cache/ClearCache.scala
@@ -2,6 +2,8 @@ package ir.sndu.server.command.cache
 
 import java.io.File
 
+import ir.sndu.server.CliConfigs
+import ir.sndu.server.ElitemConsole.withWarning
 import ir.sndu.server.command.CommandBase
 import ir.sndu.server.utils.FileHelper
 import picocli.CommandLine.Command
@@ -11,8 +13,10 @@ import picocli.CommandLine.Command
   description = Array("Clear DB"))
 class ClearCache extends CommandBase {
 
+  private def getPath: String = Option(parentCmd.database).getOrElse(CliConfigs.dbPath + "/session")
+
   override def run(): Unit = {
-    FileHelper.deleteRecursively(new File("./session"))
+    FileHelper.deleteRecursively(new File(getPath))
   }
 }
 
