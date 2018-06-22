27879a646fc4cacdea1ce56dd449c2b1a907d4eb
AmirHossein Javan <amsjavan@gmail.com>
2018-06-22 12:48:45 +0430

    fix(cli): Fixes from uniqueId alg

--- elitem-cli/src/main/scala/ir/sndu/server/PeerHelper.scala
+++ elitem-cli/src/main/scala/ir/sndu/server/PeerHelper.scala
@@ -5,8 +5,8 @@ import ir.sndu.server.peer.{ ApiPeer, ApiPeerType }
 object PeerHelper {
   def toUniqueId(peer: ApiPeer): Long = (peer.id * Math.pow(2, 32) + peer.`type`.value).toLong
   def fromUniqueId(uniqueId: Long): ApiPeer = {
-    val peerId = uniqueId & Int.MaxValue
-    val peerType = uniqueId >>> 32
+    val peerType = uniqueId & Int.MaxValue
+    val peerId = uniqueId >>> 32
     ApiPeer(ApiPeerType.fromValue(peerType.toInt), peerId.toInt)
   }
 }
