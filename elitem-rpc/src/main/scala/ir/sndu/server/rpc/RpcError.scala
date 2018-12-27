fa03efa907ab46e08874fabe2ccf366a86da98ab
AmirHossein Javan <amsjavan@gmail.com>
2018-12-28 00:05:18 +0330

    fix: Fixes rpc error constructor

--- elitem-rpc/src/main/scala/ir/sndu/server/rpc/RpcError.scala
+++ elitem-rpc/src/main/scala/ir/sndu/server/rpc/RpcError.scala
@@ -4,9 +4,8 @@ import io.grpc._
 
 case class RpcError(status: Status, tag: String, description: String) extends StatusRuntimeException(
   status.withDescription(description),
-  new MetadataBuilder().put(Constant.TAG_METADATA_KEY, tag).build) {
+  new MetadataBuilder().put(Constant.TAG_METADATA_KEY, tag).build)
 
-  def this(status: Status, tag: String) = this(status, tag, null)
-
+object RpcError {
+  def apply(status: Status, tag: String): RpcError = new RpcError(status, tag, null)
 }
-
