e17070442f43ba627beb5453f3d5c7ec3f718249
AmirHossein Javan <amsjavan@gmail.com>
2019-01-21 02:22:53 +0330

    feature: Uses akka-grpc instead of scalapb-grpc

--- ghasedak-rpc/src/main/scala/im/ghasedak/server/frontend/GrpcFrontend.scala
+++ ghasedak-rpc/src/main/scala/im/ghasedak/server/frontend/GrpcFrontend.scala
@@ -1,28 +1,53 @@
 package im.ghasedak.server.frontend
 
+import akka.actor.ActorSystem
 import io.grpc._
 import org.slf4j.LoggerFactory
+import akka.http.scaladsl.UseHttp2.Always
+import akka.http.scaladsl.model.{ HttpRequest, HttpResponse }
+import akka.http.scaladsl.{ Http, HttpConnectionContext }
+import akka.stream.Materializer
+
+import scala.concurrent.{ ExecutionContext, Future }
 
 object GrpcFrontend {
 
   private var server: Option[Server] = None
   private val logger = LoggerFactory.getLogger(getClass)
 
-  def start(host: String, port: Int, services: Seq[ServerServiceDefinition]): Unit = {
-    val serverBuilder = ServerBuilder.forPort(port)
+  def start(host: String, port: Int, services: HttpRequest ⇒ Future[HttpResponse])(
+    implicit
+    system: ActorSystem,
+    mat:    Materializer): Future[Http.ServerBinding] = {
+    implicit val ec = system.dispatcher
 
-    services foreach serverBuilder.addService
+    val bound = Http().bindAndHandleAsync(
+      services,
+      interface = host,
+      port = port,
+      connectionContext = HttpConnectionContext(http2 = Always))
 
-    serverBuilder.intercept(new LoggingServerInterceptor).intercept(new TokenServerInterceptor)
-
-    server = Some(serverBuilder.build().start)
-    logger.info("Server started, listening on " + port)
-
+    //    val serverBuilder = ServerBuilder.forPort(port)
+    //
+    //    services foreach serverBuilder.addService
+    //
+    //    serverBuilder.intercept(new LoggingServerInterceptor).intercept(new TokenServerInterceptor)
+    //
+    //    server = Some(serverBuilder.build().start)
+    //    logger.info("Server started, listening on " + port)
+    //
     sys.addShutdownHook {
       logger.error("*** shutting down gRPC server since JVM is shutting down")
       stop()
       logger.error("*** server shut down")
     }
+
+    bound.foreach { binding ⇒
+      println(s"gRPC server bound to: ${binding.localAddress}")
+    }
+
+    bound
+
   }
 
   private def stop(): Unit = {
--- ghasedak-runtime/src/main/scala/im/ghasedak/server/GhasedakServerBuilder.scala
+++ ghasedak-runtime/src/main/scala/im/ghasedak/server/GhasedakServerBuilder.scala
@@ -2,12 +2,13 @@ package im.ghasedak.server
 
 import akka.actor.ActorSystem
 import akka.cluster.Cluster
+import akka.stream.{ ActorMaterializer, Materializer }
 import com.typesafe.config.Config
-import im.ghasedak.rpc.auth.AuthServiceGrpc
-import im.ghasedak.rpc.contact.ContactServiceGrpc
-import im.ghasedak.rpc.messaging.MessagingServiceGrpc
-import im.ghasedak.rpc.test.TestServiceGrpc
-import im.ghasedak.rpc.user.UserServiceGrpc
+import im.ghasedak.rpc.auth.AuthServiceHandler
+import im.ghasedak.rpc.contact.ContactServiceHandler
+import im.ghasedak.rpc.messaging.MessagingServiceHandler
+import im.ghasedak.rpc.test.TestServiceHandler
+import im.ghasedak.rpc.user.UserServiceHandler
 import io.grpc.ServerServiceDefinition
 import im.ghasedak.server.frontend.Frontend
 import im.ghasedak.server.rpc.auth.AuthServiceImpl
@@ -16,11 +17,15 @@ import im.ghasedak.server.rpc.messaging.MessagingServiceImpl
 import im.ghasedak.server.rpc.test.TestServiceImpl
 import im.ghasedak.server.rpc.user.UserServiceImpl
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ ExecutionContext, Future }
+import akka.http.scaladsl.UseHttp2.Always
+import akka.http.scaladsl.model.{ HttpRequest, HttpResponse }
+import akka.http.scaladsl.{ Http, HttpConnectionContext }
+import akka.grpc.scaladsl.ServiceHandler
 
 object GhasedakServerBuilder {
 
-  def start(config: Config): ActorSystem = {
+  def start(implicit config: Config): ActorSystem = {
     implicit val system: ActorSystem =
       ActorSystem(config.getString("server-name"), config)
 
@@ -29,8 +34,9 @@ object GhasedakServerBuilder {
     }
 
     implicit val ex: ExecutionContext = system.dispatcher
+    implicit val mat: Materializer = ActorMaterializer()
 
-    Frontend.start(ServiceDescriptors.services)(config)
+    Frontend.start(ServiceDescriptors.services)
 
     system
   }
@@ -39,13 +45,21 @@ object GhasedakServerBuilder {
 
 object ServiceDescriptors {
 
-  def services(implicit system: ActorSystem, ec: ExecutionContext): Seq[ServerServiceDefinition] = {
-    Seq(
-      TestServiceGrpc.bindService(new TestServiceImpl, ec),
-      AuthServiceGrpc.bindService(new AuthServiceImpl, ec),
-      MessagingServiceGrpc.bindService(new MessagingServiceImpl, ec),
-      ContactServiceGrpc.bindService(new ContactServiceImpl(), ec),
-      UserServiceGrpc.bindService(new UserServiceImpl(), ec))
+  def services(implicit system: ActorSystem, ec: ExecutionContext, mat: Materializer): HttpRequest ⇒ Future[HttpResponse] = {
+    // explicit types not needed but included in example for clarity
+    val testService: PartialFunction[HttpRequest, Future[HttpResponse]] =
+      TestServiceHandler.partial(new TestServiceImpl)
+    val authService: PartialFunction[HttpRequest, Future[HttpResponse]] =
+      AuthServiceHandler.partial(new AuthServiceImpl)
+    val messagingService: PartialFunction[HttpRequest, Future[HttpResponse]] =
+      MessagingServiceHandler.partial(new MessagingServiceImpl)
+    val contactService: PartialFunction[HttpRequest, Future[HttpResponse]] =
+      ContactServiceHandler.partial(new ContactServiceImpl)
+    val userService: PartialFunction[HttpRequest, Future[HttpResponse]] =
+      UserServiceHandler.partial(new UserServiceImpl)
+    val serviceHandlers: HttpRequest ⇒ Future[HttpResponse] =
+      ServiceHandler.concatOrNotFound(testService, authService, messagingService, contactService, userService)
+    serviceHandlers
   }
 
 }
