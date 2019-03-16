c1ed7c48f4cb810a2157406f6706c21f55b327a1
AmirHossein Javan <amsjavan@gmail.com>
2019-03-16 14:21:36 +0330

    fix: Handles multiple StreamGetDifference at the same
    Delivers update to the last sender

--- ghasedak-core/src/main/scala/im/ghasedak/server/update/PulsarHelper.scala
+++ ghasedak-core/src/main/scala/im/ghasedak/server/update/PulsarHelper.scala
@@ -1,12 +1,18 @@
 package im.ghasedak.server.update
 
+import akka.stream.scaladsl.{ Keep, Source, StreamRefs }
+import akka.stream.{ ActorMaterializer, KillSwitches, SourceRef, UniqueKillSwitch }
 import com.sksamuel.pulsar4s.{ Consumer, ConsumerConfig, PulsarClient, PulsarClientConfig }
+import im.ghasedak.rpc.update.ResponseGetDifference
 import org.apache.pulsar.client.api.Schema
 
+import scala.concurrent.Future
 import scala.util.Try
 
 trait PulsarHelper {
   this: UpdateProcessor ⇒
+
+  case class SourceRefSwitch(killSwitch: UniqueKillSwitch, sourceRefFuture: Future[SourceRef[ResponseGetDifference]])
 
   protected lazy val pulsarHost: String = config.getString("module.update.pulsar.host")
   protected lazy val pulsarPort: Int = config.getInt("module.update.pulsar.port")
@@ -21,10 +27,23 @@ trait PulsarHelper {
     topics = Seq(topic))
 
   protected var consumer: Option[Consumer[UpdateMapping]] = None
+  protected var updateSourceRef: Option[SourceRefSwitch] = None
 
   protected def createConsumer: Consumer[UpdateMapping] = {
     Try(consumer.foreach(_.close()))
     consumer = Some(pulsarClient.consumer[UpdateMapping](consumerConfig))
     consumer.get
   }
+
+  protected def createSource(): Source[ResponseGetDifference, UniqueKillSwitch] = {
+    com.sksamuel.pulsar4s.akka.streams.committableSource(() ⇒ createConsumer, None)
+      .viaMat(KillSwitches.single)(Keep.right)
+      .map(i ⇒ buildDifference(tokenId, i.message))
+  }
+
+  protected def getSourceRef: SourceRefSwitch = {
+    val matValue = createSource().toMat(StreamRefs.sourceRef[ResponseGetDifference]())(Keep.both).run()(mat)
+    SourceRefSwitch(matValue._1, matValue._2)
+  }
+
 }
--- ghasedak-core/src/main/scala/im/ghasedak/server/update/UpdateProcessor.scala
+++ ghasedak-core/src/main/scala/im/ghasedak/server/update/UpdateProcessor.scala
@@ -3,23 +3,20 @@ package im.ghasedak.server.update
 import akka.Done
 import akka.actor.ActorSystem
 import akka.actor.typed.scaladsl.adapter._
-import akka.actor.typed.scaladsl.{ AbstractBehavior, ActorContext, Behaviors }
+import akka.actor.typed.scaladsl.{ ActorContext, Behaviors }
 import akka.actor.typed.{ Behavior, PostStop, Signal }
 import akka.cluster.sharding.typed.scaladsl.EntityTypeKey
 import akka.event.Logging
-import akka.stream.{ ActorMaterializer, SourceRef }
-import akka.stream.scaladsl.StreamRefs
+import akka.stream.ActorMaterializer
 import com.typesafe.config.Config
-import im.ghasedak.rpc.update.ResponseGetDifference
 import im.ghasedak.server.Processor
 import im.ghasedak.server.db.DbExtension
+import im.ghasedak.server.serializer.ActorRefConversions._
 import im.ghasedak.server.serializer.ImplicitActorRef._
 import im.ghasedak.server.update.UpdateEnvelope.{ Acknowledge, StreamGetDifference, Subscribe }
-import im.ghasedak.server.update.UpdateProcessor.StopOffice
-import org.apache.pulsar.client.api.Schema
 import slick.jdbc.PostgresProfile
 
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.ExecutionContext
 
 object UpdateProcessor {
   val ShardingTypeName: EntityTypeKey[UpdatePayload] = EntityTypeKey[UpdatePayload]("UpdateProcessor")
@@ -34,11 +31,14 @@ object UpdateProcessor {
 class UpdateProcessor(context: ActorContext[UpdatePayload], entityId: String) extends Processor[UpdatePayload]
   with UpdateHelper
   with PulsarHelper {
-  private implicit val system: ActorSystem = context.system.toUntyped
-  private implicit val mat = ActorMaterializer()
-  private implicit val ec: ExecutionContext = system.dispatcher
-  private implicit val db: PostgresProfile.backend.Database = DbExtension(system).db
-  private val log = Logging(system, getClass)
+
+  import UpdateProcessor._
+
+  protected implicit val system: ActorSystem = context.system.toUntyped
+  protected implicit val mat: ActorMaterializer = ActorMaterializer()
+  protected implicit val ec: ExecutionContext = system.dispatcher
+  protected implicit val db: PostgresProfile.backend.Database = DbExtension(system).db
+  protected val log = Logging(system, getClass)
 
   protected val config: Config = system.settings.config
 
@@ -50,22 +50,19 @@ class UpdateProcessor(context: ActorContext[UpdatePayload], entityId: String) ex
 
   }
 
-  import im.ghasedak.server.serializer.ActorRefConversions._
+  initialize()
 
   override def onReceive: Receive = {
     case s: StreamGetDifference ⇒
-      val src = com.sksamuel.pulsar4s.akka.streams.committableSource(() ⇒ createConsumer, None)
-        .map(i ⇒ buildDifference(tokenId, i.message))
+      updateSourceRef.foreach(_.killSwitch.shutdown())
+      updateSourceRef = Some(getSourceRef)
+      updateSourceRef.foreach(_.sourceRefFuture pipeTo s.replyTo)
 
-      val ref: Future[SourceRef[ResponseGetDifference]] = src.runWith(StreamRefs.sourceRef[ResponseGetDifference]())
-
-      ref pipeTo s.replyTo
       Behaviors.same
 
     case s: Subscribe ⇒
-      createConsumer
-
       s.replyTo ! Done
+
       Behaviors.same
 
     case Acknowledge(replyTo, ack) ⇒
@@ -74,7 +71,7 @@ class UpdateProcessor(context: ActorContext[UpdatePayload], entityId: String) ex
       Behaviors.same
 
     case StopOffice ⇒
-      log.debug("Stopping ......")
+      log.debug("Stopping ...")
       Behaviors.stopped
     case _ ⇒
       Behaviors.unhandled
@@ -82,8 +79,16 @@ class UpdateProcessor(context: ActorContext[UpdatePayload], entityId: String) ex
 
   override def onSignal: PartialFunction[Signal, Behavior[UpdatePayload]] = {
     case PostStop ⇒
-      context.log.info("UpdateProcessor  stopped")
+      log.info("UpdateProcessor was stopped")
       consumer.foreach(_.close())
       this
   }
+
+  /*
+    Will be called after actor initialized
+   */
+  private def initialize(): Unit = {
+    createConsumer
+  }
+
 }
